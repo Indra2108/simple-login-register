c9970e043e837514c1397d51f875ff0d66f369d9
Indra2108 <fizipratama21@gmail.com>
2021-11-02 13:10:18 +0700

    Menambahkan Login Screen

---
+++ src/login/login.js
@@ -0,0 +1,29 @@
+import React, {Component} from "react";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import style from "./style";
+
+export default class Login extends Component {
+    render() {
+        return(
+            <View style={style.container}>
+                <TextInput 
+                    placeholder='Username'
+                    style={style.textinput}
+                />
+
+                <TextInput 
+                    placeholder='Password'
+                    style={style.textinput}
+                />
+
+                <TouchableOpacity style={style.tombol}>
+                    <Text>LOGIN</Text>
+                </TouchableOpacity>
+                
+                <TouchableOpacity style={style.tombol}>
+                    <Text>REGISTER</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+}
---
+++ src/login/style.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+
+export default style = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    textinput: {
+        padding: 10,
+        borderWidth: 1,
+        borderRadius: 10
+    },
+    tombol: {
+        width: '100%',
+        height: '10%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        color: 'blue',
+        borderRadius: 10
+    },
+})
