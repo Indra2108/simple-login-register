3cf28aa582e113c74d27326484f6673165012e18
Indra2108 <fizipratama21@gmail.com>
2021-11-04 13:30:41 +0700

    Menambahkan Register screen

--- src/register/index.js
+++ src/register/index.js
@@ -5,7 +5,28 @@ export default class Register extends Component {
     render() {
         return(
             <View style={Style.container}>
-                <Text>INI REGISTER</Text>
+                <View style={Style.inputbackground}>
+                    <TextInput 
+                        placeholder='Username'
+                        style={Style.textinput}
+                    />
+                </View>
+
+                <View style={Style.inputbackground}>
+                    <TextInput 
+                        placeholder='Password'
+                        style={Style.textinput}
+                    />
+                </View>
+
+                <TouchableOpacity style={Style.tombol}>
+                    <Text style={Style.tulisantombol}>Register</Text>
+                </TouchableOpacity>
+
+                <View  style={Style.teksregister}>
+                    <Text>Sudah punya akun? </Text>
+                    <Text onPress={() => this.props.navigation.navigate('Login')} style={Style.register}>Log In</Text>
+                </View>
             </View>
         )
     }
