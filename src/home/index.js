5f2f396f0b5324e3495c82d4a4421c5abd3c485e
Indra2108 <fizipratama21@gmail.com>
2021-11-04 13:27:13 +0700

    Remove unecessary things

--- src/home/index.js
+++ src/home/index.js
@@ -5,10 +5,6 @@ import {View, TouchableOpacity, StyleSheet, Text} from 'react-native';
 import Style from "./Style";
 
 export default class Home extends Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return(
             <View style={Style.container}>
