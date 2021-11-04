e7cc9175a6afb465f2e5f923c1aebf5cc6d4053b
Indra2108 <fizipratama21@gmail.com>
2021-11-04 13:37:48 +0700

    Remove unnecessary component - StyleSheet

--- src/home/index.js
+++ src/home/index.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {View, TouchableOpacity, StyleSheet, Text} from 'react-native';
+import {View, TouchableOpacity, Text} from 'react-native';
 
 // IMPORT STYLE
 import Style from "./Style";
--- src/register/index.js
+++ src/register/index.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from "react-native";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
 
 // IMPORT STYLE
 import Style from "./Style";
